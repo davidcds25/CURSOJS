d9d69aa277296331b214557552e0f7545db0ce31
davidcds25 <deezinft@gmail.com>
2024-04-18 14:32:29 -0300

    CursoJs

---
+++ aula30 - Switch case/main.js
@@ -0,0 +1,93 @@
+// Criando uma função com switch case
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+
+   switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        return diaSemanaTexto;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        return diaSemanaTexto;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        return diaSemanaTexto;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        return diaSemanaTexto;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        return diaSemanaTexto;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        return diaSemanaTexto;
+    case 6:
+        diaSemanaTexto = 'Sábado';
+        return diaSemanaTexto;
+    default:
+        diaSemanaTexto = 'Inválido';
+        return diaSemanaTexto;
+    } 
+
+  //  return diaSemanaTexto;
+}
+
+
+const data = new Date('1987-04-25 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+
+
+
+console.log(diaSemana, diaSemanaTexto);
+
+
+
+// Usando o IF
+// if (diaSemana === 0) {
+//     diaSemanaTexto = 'Domingo';
+// } else if (diaSemana === 1) {
+//     diaSemanaTexto = 'Segunda';
+// } else if (diaSemana === 2) {
+//     diaSemanaTexto = 'Terça';
+// } else if (diaSemana === 3) {
+//     diaSemanaTexto = 'Quarta';
+// } else if (diaSemana === 4) {
+//     diaSemanaTexto = 'Quinta';
+// } else if (diaSemana === 5) {
+//     diaSemanaTexto = 'Sexta';
+// } else if (diaSemana === 6) {
+//     diaSemanaTexto = 'Sabado';
+// } else {
+//     diaSemanaTexto = 'Inválido';
+// }
+
+// Usando o Switch
+// switch (diaSemana) {
+//     case 0:
+//         diaSemanaTexto = 'Domingo';
+//         break;
+//     case 1:
+//         diaSemanaTexto = 'Segunda';
+//         break;
+//     case 2:
+//         diaSemanaTexto = 'Terça';
+//         break;
+//     case 3:
+//         diaSemanaTexto = 'Quarta';
+//         break;
+//     case 4:
+//         diaSemanaTexto = 'Quinta';
+//         break;
+//     case 5:
+//         diaSemanaTexto = 'Sexta';
+//         break;
+//     case 6:
+//         diaSemanaTexto = 'Sábado';
+//         break;
+//     default:
+//         diaSemanaTexto = 'Inválido';
+//         break;
+//     }
+
